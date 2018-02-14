73fbb6ba39dc7ac99273a0a5475c5b5f6e604d63
Gintas <maziukas2008@gmail.com>
2018-02-13 21:18:58 -0600

    update to user module
    fixing issue. user table had many Emotion (not EmotionS)

--- models/user.js
+++ models/user.js
@@ -52,7 +52,7 @@ module.exports = function(sequelize, DataTypes) {
   User.associate = function(models) {
     // We're saying that a Post should belong to an Author
     // A Post can't be created without an Author due to the foreign key constraint
-    User.hasMany(models.Emotions, {
+    User.hasMany(models.Emotion, {
       foreignKey: {
         allowNull: false
       }
