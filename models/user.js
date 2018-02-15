b2381bbdc5a1a4aa495f3b60d6c8592d1ebecd97
Laura Wentzell Ahmad <laura.s.ahmad@gmail.com>
2018-02-15 14:46:11 -0600

    adding sql token

--- models/user.js
+++ models/user.js
@@ -36,7 +36,11 @@ module.exports = function(sequelize, DataTypes) {
       // validate:{
       //   len:[5,40]
       // }
-    }
+    },
+    //  token: {
+    //   type: DataTypes.STRING,
+    //   allowNull: true
+    // }
 
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
