da8e659b083b737dd57c6f4daa7e7a5b27453606
Rescarra <ryn.b.escarra@gmail.com>
2018-02-17 14:52:57 -0600

    css edits; memo progress push

--- public/memos/memos.js
+++ public/memos/memos.js
@@ -3,11 +3,14 @@
 /*==================================================================*/
 'use strict';
 $(function() {
-	
+	 
 	// Global Variables
 	/*-------------------------------------------------- */
 	let user_id;
+	let username;
+	let colorToday;
 	let today = moment().format('MM/DD/YYYY');
+	let today2 = moment().format("dddd, MMMM Do YYYY")
 	
 	// Navbar Dropdown
 	/*-------------------------------------------------- */
@@ -17,7 +20,23 @@ $(function() {
 	/*-------------------------------------------------- */
 	$('.collapsible').collapsible();
 	$('.memoList').collapsible('open', 0);
-	
+	 
+	// $('.modal').modal();
+	// $('.modal').modal({
+	//     dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	//     opacity: .5, // Opacity of modal background
+	//     inDuration: 300, // Transition in duration
+	//     outDuration: 200, // Transition out duration
+	//     startingTop: '4%', // Starting top style attribute
+	//     endingTop: '10%', // Ending top style attribute
+	//   //   ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+	//   //     alert("Ready");
+	//   //     console.log(modal, trigger);
+	//   //   },
+	//   //   complete: function() { alert('Closed'); } // Callback for Modal close
+	//   }
+	// );
+	// $('#modal1').modal('open');
 
 /*==================================================================*/
 //  <--> OPERATIONS <-->
@@ -27,27 +46,57 @@ $(function() {
 	//--------------------------------------------------
 	$.get("/api/user_data").then(function(data) {
 		user_id = data.id;
-
+		username = data.username;
+		console.log(username);
+		$(".modal-header").text(username);
+		
+		$.get("/api/emotions", {
+			user_id: user_id
+		}).then(function(data) {
+			console.log(data)
+			for (let i = 0; i < data.length; i++) {
+				var emoDateToPush = moment(data[i].Emotion_Date,"YYYY-MM-DD").format("dddd, MMMM Do YYYY");
+				console.log(emoDateToPush);
+				console.log(today2);
+				// if(emoDateToPush === today2){
+					colorToday = data[i].Color
+					getMemos(colorToday);
+				// }
+			}
+		});
 		// GET  <- request user memos from DB
 		//--------------------------------------------------
+		function getMemos(colorToday){
 		$.get("/api/memos", {
 			user_id: user_id
 		}).then(function(data) {
 			let memoDates = []
 			for (let i = 0; i < data.length; i++) {
+				var memoDateToPush = moment(data[i].Memo_Date,"YYYY-MM-DD").format("dddd, MMMM Do YYYY");
+
+				if(memoDates.includes(memoDateToPush) === true) {
+					console.log('nope');
+				}
+				else {
 			    memoDates.push(moment(data[i].Memo_Date,"YYYY-MM-DD").format("dddd, MMMM Do YYYY"));
-			    memoDates.push(moment(data[i].Memo_Date,"YYYY-MM-DD").format("M/D/YYYY"))
+				}
+			    memoDates.push(moment(data[i].Memo_Date,"YYYY-MM-DD").format("M/D/YYYY"));
+			    // var memoDate = data[i].Memo_Date;
+			    // var memoID = data[i].id;
+			    // var memoText = data[i].Memo_Text;
+			    // console.log('ID: ' + memoID + ' | ' + memoDate + '  | ' + memoText );
 			}
-			viewActiveMemos(data, memoDates);
-
-			if($.inArray(moment().format("M/D/YYYY"),memoDates)>-1) {
-			  $("#submit-memo").addClass("disabled");
-			  // $(".memo").text('CHECK BACK IN TOMORROW TO LOG ANOTHER MEMO');
-			}
+			viewActiveMemos(data, memoDates, colorToday);
 			
-		});
-	});
 
+			// if($.inArray(moment().format("dddd, MMMM Do YYYY"),memoDates)>-1) {
+			//   $("#submit-memo").addClass("disabled");
+			//   // $(".memo").text('CHECK BACK IN TOMORROW TO LOG ANOTHER MEMO');
+			// }
+			
+		}); 
+	};
+});
 	// Submit Memo
 	//--------------------------------------------------
 	$(document).on("click", "#submit-memo", function() {
@@ -81,21 +130,36 @@ $(function() {
 
 	// Render memos in collapsible component
 	//--------------------------------------------------
-	function viewActiveMemos(data, memoDates) {
+	function viewActiveMemos(data, memoDates, colorToday) {
 		console.log(data);
+		console.log(memoDates);
+		console.log(colorToday);
 		var memoList = $(".memoList").addClass('collapsible popout');
+		// var memoDateToPush = moment(data[j].Memo_Date, "YYYY-MM-DD").format("dddd, MMMM Do YYYY")
 
+		
 		for (var j = 0; j < data.length; j++) {
-			var memoListItems = $("<li>").attr('list-id', data[j].id).addClass(
-				'memoListItems indigo lighten-4 z-depth-5')
+			var memoListItems = $("<li>").attr('list-id', data[j].id).addClass('memoListItems indigo lighten-4 z-depth-5');
 			
 			var memoDateToShow = $('<div class="collapsible-header"><h6 class="memoDate">').html(moment(data[j].Memo_Date, "YYYY-MM-DD").format("dddd, MMMM Do YYYY"));
 
 			var memoTextToShow = $('<div class="collapsible-body"><span class="memoText">').html(data[j].Memo_Text);
+
+			// var btnColor = $("<button class='btn btn-color inline'>").css('background', colorToday);
+					// $(".modal-color").append(btnColor);
+			// if(emoDateToPush === memoDateToShow){
+			// 	console.log('blah!' + emoDateToPush + " " + memoDateToPush)
+			// 	memoTextToShow.css('background', colorToday)
+			// }
+			// else{console.log('blerg!' + emoDateToPush + " " + memoDateToPush)}
+
 			memoListItems.append(memoDateToShow, memoTextToShow);
 			memoList.prepend(memoListItems);
 		}
 	}
 
-
+	// $(".collapsible-header").on("click", function(){
+	// 	var currentMemo = $(this)
+	// 	console.log(currentMemo)
+	// })
 }); // onready closure
