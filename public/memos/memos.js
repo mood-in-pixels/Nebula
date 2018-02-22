f32419c6ddb9e4136707b8ebe08755ad972d4a3a
Gintas <maziukas2008@gmail.com>
2018-02-21 21:06:04 -0600

    updating memos

--- public/memos/memos.js
+++ public/memos/memos.js
@@ -3,7 +3,7 @@
 /*==================================================================*/
 'use strict';
 $(function() {
-	 
+
 	// Global Variables
 	/*-------------------------------------------------- */
 	let user_id;
@@ -11,16 +11,16 @@ $(function() {
 	let colorToday;
 	let today = moment().format('MM/DD/YYYY');
 	let today2 = moment().format("dddd, MMMM Do YYYY")
-	
+
 	// Navbar Dropdown
 	/*-------------------------------------------------- */
 	$(".dropdown-button").dropdown();
-	
+
 	// Collapse Memo Collection
 	/*-------------------------------------------------- */
 	$('.collapsible').collapsible();
 	$('.memoList').collapsible('open', 0);
-	 
+
 
 /*==================================================================*/
 //  <--> OPERATIONS <-->
@@ -33,7 +33,7 @@ $(function() {
 		username = data.username;
 		console.log(username);
 		$(".modal-header").text(username);
-		
+
 		$.get("/api/emotions", {
 			user_id: user_id
 		}).then(function(data) {
@@ -65,11 +65,11 @@ $(function() {
 			    memoDates.push(moment(data[i].Memo_Date,"YYYY-MM-DD").format("dddd, MMMM Do YYYY"));
 				}
 			    memoDates.push(moment(data[i].Memo_Date,"YYYY-MM-DD").format("M/D/YYYY"));
-			  
+
 			}
 			viewActiveMemos(data, memoDates, colorToday);
-			
-		}); 
+
+		});
 	};
 });
 	// Submit Memo
@@ -88,7 +88,7 @@ $(function() {
 		location.reload();
 	});
 
-	// POST -> memos to DB 
+	// POST -> memos to DB
 	//--------------------------------------------------
 	function logDailyMemos(user_id, memo_text, memo_date) {
 		$.post("/api/memos", {
@@ -96,7 +96,7 @@ $(function() {
 			memo_text: memo_text,
 			memo_date: memo_date
 		}).then(function() {
-			window.location.replace(data);
+			console.log("memo recorded");
 			// If there's an error, log the error
 		}).catch(function(err) {
 			console.log(err);
@@ -112,10 +112,10 @@ $(function() {
 		var memoList = $(".memoList").addClass('collapsible popout');
 		// var memoDateToPush = moment(data[j].Memo_Date, "YYYY-MM-DD").format("dddd, MMMM Do YYYY")
 
-		
+
 		for (var j = 0; j < data.length; j++) {
 			var memoListItems = $("<li>").attr('list-id', data[j].id).addClass('memoListItems indigo lighten-4 z-depth-5');
-			
+
 			var memoDateToShow = $('<div class="collapsible-header"><h6 class="memoDate">').html(moment(data[j].Memo_Date, "YYYY-MM-DD").format("dddd, MMMM Do YYYY"));
 
 			var memoTextToShow = $('<div class="collapsible-body"><span class="memoText">').html(data[j].Memo_Text);
@@ -124,5 +124,43 @@ $(function() {
 			memoList.prepend(memoListItems);
 		}
 	}
+
+
+
+	function seedMemos(){
+		logDailyMemos(user_id, 'I wonder what this new year will bring. 2017 were not so great to me' , '1/1/2018')
+		logDailyMemos(user_id, 'What a great book, I finally finished it.' , '1/3/2018')
+		logDailyMemos(user_id, 'I feel great. I volunteered today at a dog shelter. Things are looking good.' , '1/7/2018')
+		logDailyMemos(user_id, 'My dog died… :(' , '1/11/2018')
+		logDailyMemos(user_id, 'I miss my dog. :(' , '1/13/2018')
+		logDailyMemos(user_id, 'I spent 2 hours looking at my dogs pictures. I miss him... I have so many great memories to treasure.' , '1/15/2018')
+		logDailyMemos(user_id, 'Busy Busy Busy day….' , '1/19/2018')
+		logDailyMemos(user_id, 'Saturday!!!!!!!! I finally caught up on my sleep.' , '1/20/2018')
+		logDailyMemos(user_id, 'I met my old friend from high school. ' , '1/25/2018')
+		logDailyMemos(user_id, 'I can't believe January is almost over. Only 2 more months of winter…' , '1/31/2018')
+		logDailyMemos(user_id, 'Yoga class was amazing…' , '2/5/2018')
+		logDailyMemos(user_id, 'Started new mission… I will try drink less soda… wish me luck future me.' , '2/4/2018')
+		logDailyMemos(user_id, 'All these skinny people… ' , '2/6/2018')
+		logDailyMemos(user_id, 'Finally I am seeing results of my time spent at the gym. I feel great' , '2/13/2018')
+		logDailyMemos(user_id, 'I helped people today…' , '2/15/2018')
+		logDailyMemos(user_id, 'OMG this app is great. Thank you NEBULA… ;)' , '2/18/2018')
+		logDailyMemos(user_id, 'Heroku is not working, lets see what tomorrow brings…' , '2/23/2018')
+
+
+	}
+
+
+
+	$(document).on("click", "#seed", function() {
+	  seedMemos()
+	})
+
+
+
+
+
+
+
+
 
 }); // onready closure
